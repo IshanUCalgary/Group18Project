71f2435871beb646c182f882c77f1c33f2d74ed8
IshanUCalgary <ishan.vyas1@ucalgary.ca>
2019-03-25 08:23:32 -0600

    Update

---
+++ Project/src/engProjectile.java
@@ -0,0 +1,13 @@
+
+public class engProjectile extends Projectile {
+
+	public engProjectile(ProjectileType type, Enemy target, float x, float y, int width, int height) {
+		super(type, target, x, y, width, height);
+	}
+	
+	@Override
+	public void damage() {
+		super.damage();
+	}
+
+}
